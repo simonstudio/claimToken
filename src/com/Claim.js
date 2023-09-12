618955122959cc1a5912617295bb097eb7427ba9
Simon <simon72217@gmail.com>
2023-09-12 22:49:00 +0700

    a

--- src/com/Claim.js
+++ src/com/Claim.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withTranslation } from 'react-i18next';
-import { Col, Row, } from 'antd';
+import { Card, Col, Row, } from 'antd';
 import { connect } from 'react-redux';
 import "./Claim.scss"
 
@@ -10,6 +10,7 @@ const style = {
 
 class Claim extends React.Component {
     state = {
+
     }
 
     componentDidMount() {
@@ -21,119 +22,128 @@ class Claim extends React.Component {
         let { t } = this.props
 
         return (
-            <Row>
-                <Col span={12} offset={6}>
-                    <div style={style}>
-                        <h5 className="hr-h5">{t("TIME REMAINING")}</h5>
-                        <div className="clock hr-mt-5">
-                            <div className="digit">
-                                <span id="days">0</span>
-                                <div className="label">Days</div>
-                            </div>
-                            <div className="digit">
-                                <span id="hours">0</span>
-                                <div className="label">Hours</div>
-                            </div>
-                            <div className="digit">
-                                <span id="minutes">0</span>
-                                <div className="label">Minutes</div>
-                            </div>
-                            <div className="digit">
-                                <span id="seconds">0</span>
-                                <div className="label">Seconds</div>
-                            </div>
-                        </div>
-
-
-                        <div className="hr-form">
-                            <div className="row">
-                                <div className="col-6 text-1">Buy HAR Now</div>
-                                <div className="col-6">
-                                    <div className="text-2" style={{ alignItems: "center", display: "flex", justifyContent: "flex-end" }}>
-                                        0x0000....000000
-                                        <img src="images/ic_brower.png" alt="Hình ảnh" style={{ width: "13px", height: "13px" }} />
-                                    </div>
-                                </div>
-
-                                <div className="mt-3 har-control">
-                                    <div className="row">
-                                        <div className="col-8">
-                                            <input id="inputIn" type="number" className="custom-input" placeholder="0" />
-                                        </div>
-                                        <div className="col-4">
-                                            <div className="d-flex justify-content-end align-items-center">
-                                                <img src="images/ic_utsd.png" alt="tusd"
-                                                    style={{ width: "22px", height: "22px", verticalAlign: "middle" }} />
-                                                <div className="input-text ml-1">
-                                                    USDT
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-12 custom-text-1">
-                                            Balance: <span>0</span> | <span>Half</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-12 btn_swap_container">
-                                    <img src="images/ic_swap.png" className="btn_swap" alt="swap" />
-                                </div>
-                                <div className="har-control">
-                                    <div className="row">
-                                        <div className="col-8">
-                                            <input id="inputOut" type="number" className="custom-input" placeholder="0" readOnly />
-                                        </div>
-                                        <div className="col-4">
-                                            <div className="d-flex justify-content-end align-items-center">
-                                                <img src="images/ic_har.png" alt="tusd"
-                                                    style={{ width: "22px", height: "22px", verticalAlign: "middle" }} />
-                                                <div className="input-text ml-1">
-                                                    HAR
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-12 custom-text-1">
-                                            Balance: <span>0</span>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="col-3 mt-2 text-3">Rate</div>
-                                <div className="col-9 mt-2 text-4">1 USDT = 2,000 HAR</div>
-
-                                <div className="col-6 mt-2 text-3">Slippage Tolerance</div>
-                                <div className="col-6 mt-2 text-4">0% </div>
-
-                                <div className="col-5 mt-2 text-3">Symbol: HAR</div>
-                                <div className="col-7 mt-2">
-                                    <div className="text-5" style={{ alignItems: "center", display: "flex", justifyContent: "flex-end" }} >
-                                        Import HAR Token
-                                        <img src="images/ic_brower.png" alt="Hình ảnh" style={{ width: "13px", height: "13px" }} />
-                                    </div>
-                                </div>
-
-                                <div className="col-8 mt-2 text-3">Generate your referral code</div>
-                                <div className="col-4 mt-2 text-5">
-                                    <div className="text-5" style={{ alignItems: "center", display: "flex", justifyContent: "flex-end" }}>
-                                        Copy
-                                        <img src="images/ic_copy.png" alt="Hình ảnh" style={{ width: "13px", height: "13px" }} />
-                                    </div>
-                                </div>
-
-                                <div className="col-8 mt-2 text-3">Route</div>
-                                <div className="col-4 mt-2 text-5"></div>
-
-                                <div className="col-12 ">
-                                    <a href="#" className="btn-conenct-wallet btn">
-                                        Connect Wallet
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-
-
+            <Card bordered={false} style={{ maxWidth: 375, margin: "auto" }}>
+                {/* <div style={style}> */}
+                <h5 className="hr-h5">{t("TIME REMAINING")}</h5>
+                <div className="clock hr-mt-5">
+                    <div className="digit">
+                        <span id="days">0</span>
+                        <div className="label">Days</div>
                     </div>
-                </Col>
-            </Row >
+                    <div className="digit">
+                        <span id="hours">0</span>
+                        <div className="label">Hours</div>
+                    </div>
+                    <div className="digit">
+                        <span id="minutes">0</span>
+                        <div className="label">Minutes</div>
+                    </div>
+                    <div className="digit">
+                        <span id="seconds">0</span>
+                        <div className="label">Seconds</div>
+                    </div>
+                </div>
+
+
+                <div className="hr-form">
+                    <div>
+                        <Col className="text-1">Buy HAR Now</Col>
+                        <Col>
+                            <div className="text-2" style={{ alignItems: "center", display: "flex", justifyContent: "flex-end" }}>
+                                0x0000....000000
+                                <img src="images/ic_brower.png" style={{ width: "13px", height: "13px" }} />
+                            </div>
+                        </Col>
+
+                        <div className="mt-3 har-control">
+                            <Row>
+                                <Col>
+                                    <input id="inputIn" type="number" className="custom-input" placeholder="0" />
+                                </Col>
+                                <Col>
+                                    <div className="d-flex justify-content-end align-items-center">
+                                        <img src="images/ic_utsd.png" alt="tusd"
+                                            style={{ width: "22px", height: "22px", verticalAlign: "middle" }} />
+                                        <div className="input-text ml-1">
+                                            USDT
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col className="custom-text-1">
+                                    Balance: <span>0</span> | <span>Half</span>
+                                </Col>
+                            </Row>
+                        </div>
+                        <Col className="btn_swap_container">
+                            <img src="images/ic_swap.png" className="btn_swap" alt="swap" />
+                        </Col>
+
+                        <div className="har-control">
+                            <Row>
+                                <Col>
+                                    <input id="inputOut" type="number" className="custom-input" placeholder="0" readOnly />
+                                </Col>
+                                <Col>
+                                    <div className="d-flex justify-content-end align-items-center">
+                                        <img src="images/ic_har.png" alt="tusd"
+                                            style={{ width: "22px", height: "22px", verticalAlign: "middle" }} />
+                                        <div className="input-text ml-1">
+                                            HAR
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col className="custom-text-1">
+                                    Balance: <span>0</span>
+                                </Col>
+                            </Row>
+                        </div>
+
+                        <Row>
+                            <Col span={12} className="text-3">Rate</Col>
+                            <Col span={12} className="text-4">1 USDT = 2,000 HAR</Col>
+                        </Row>
+
+
+                        <Row>
+                            <Col span={12} className="mt-2 text-3">{t("Slippage Tolerance")}</Col>
+                            <Col span={12} className="mt-2 text-4">0% </Col>
+                        </Row>
+
+                        <Row>
+                            <Col span={12} className="mt-2 text-3">{t("Symbol")}: HAR</Col>
+                            <Col span={12} className="mt-2">
+                                <div className="text-5" style={{ alignItems: "center", display: "flex", justifyContent: "flex-end" }} >
+                                    {t("Import")} HAR {t("Token")}
+                                    <img src="images/ic_brower.png" style={{ width: "13px", height: "13px" }} />
+                                </div>
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col span={12} className="mt-2 text-3">{t("Generate your referral code")}</Col>
+                            <Col span={12} className="mt-2 text-5">
+                                <div className="text-5" style={{ alignItems: "center", display: "flex", justifyContent: "flex-end" }}>
+                                    {t("Copy")}
+                                    <img src="images/ic_copy.png" style={{ width: "13px", height: "13px" }} />
+                                </div>
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col span={12} className="mt-2 text-3">{t("Route")}</Col>
+                            <Col span={12} className="mt-2 text-5">...</Col>
+                        </Row>
+
+                        <Row>
+                            <a href="#" className="btn-connect-wallet btn">
+                                {t("Connect Wallet")}
+                            </a>
+                        </Row>
+                    </div>
+                </div>
+
+                {/* </div> */}
+            </Card>
         )
     }
 }
