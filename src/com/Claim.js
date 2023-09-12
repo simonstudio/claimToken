640f1393e313b63fdb5a2cec8c25b20866f76b2f
Simon <simon72217@gmail.com>
2023-09-12 23:47:18 +0700

    a

--- src/com/Claim.js
+++ src/com/Claim.js
@@ -10,16 +10,52 @@ const style = {
 
 class Claim extends React.Component {
     state = {
-
+        token: { Symbol: "TK" },
     }
 
     componentDidMount() {
         let { t, i18n } = this.props;
         i18n.changeLanguage("vi");
+        this.updateClock()
+    }
+
+    updateClock() {
+        const targetDate = new Date("2023-10-31 23:59:59").getTime(); // Đặt ngày đích đến
+
+        const _updateClock = () => {
+            const currentDate = new Date().getTime();
+            const timeRemaining = targetDate - currentDate;
+
+            if (timeRemaining <= 0) {
+                document.getElementById("days").textContent = "00";
+                document.getElementById("hours").textContent = "00";
+                document.getElementById("minutes").textContent = "00";
+                document.getElementById("seconds").textContent = "00";
+                clearInterval(timerInterval); // Dừng bộ đếm khi đã qua ngày đích đến
+            } else {
+                const days = formatNumber(Math.floor(timeRemaining / (1000 * 60 * 60 * 24)));
+                const hours = formatNumber(Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+                const minutes = formatNumber(Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60)));
+                const seconds = formatNumber(Math.floor((timeRemaining % (1000 * 60)) / 1000));
+
+                document.getElementById("days").textContent = days;
+                document.getElementById("hours").textContent = hours;
+                document.getElementById("minutes").textContent = minutes;
+                document.getElementById("seconds").textContent = seconds;
+            }
+        };
+
+        const formatNumber = (number) => {
+            return number < 10 ? "0" + number : number;
+        };
+
+        // Cập nhật đồng hồ mỗi giây
+        const timerInterval = setInterval(_updateClock, 1000);
     }
 
     render() {
         let { t } = this.props
+        let { token } = this.state
 
         return (
             <Card bordered={false} style={{ maxWidth: 375, margin: "auto" }}>
@@ -46,61 +82,61 @@ class Claim extends React.Component {
 
 
                 <div className="hr-form">
-                    <div>
-                        <Col className="text-1">Buy HAR Now</Col>
-                        <Col>
-                            <div className="text-2" style={{ alignItems: "center", display: "flex", justifyContent: "flex-end" }}>
-                                0x0000....000000
-                                <img src="images/ic_brower.png" style={{ width: "13px", height: "13px" }} />
-                            </div>
-                        </Col>
+                    <div className="claim">
+                        <Row>
+                            <Col span={12} className="text-1">Buy {"Symbol"} Now</Col>
+                            <Col span={12}>
+                                <div className="text-2" style={{ alignItems: "center", display: "flex", justifyContent: "flex-end" }}>
+                                    0x0000....000000
+                                    <img src="images/ic_brower.png" style={{ width: "13px", height: "13px" }} />
+                                </div>
+                            </Col>
+                        </Row>
 
-                        <div className="mt-3 har-control">
-                            <Row>
-                                <Col>
-                                    <input id="inputIn" type="number" className="custom-input" placeholder="0" />
-                                </Col>
-                                <Col>
-                                    <div className="d-flex justify-content-end align-items-center">
-                                        <img src="images/ic_utsd.png" alt="tusd"
-                                            style={{ width: "22px", height: "22px", verticalAlign: "middle" }} />
-                                        <div className="input-text ml-1">
-                                            USDT
-                                        </div>
+                        <Row className="mt-3 har-control">
+                            <Col span={24 / 4 * 3}>
+                                <input id="inputIn" type="number" className="custom-input" placeholder="0" />
+                            </Col>
+                            <Col span={24 / 4}>
+                                <div className="d-flex justify-content-end align-items-center">
+                                    <img src="images/ic_utsd.png"
+                                        style={{ width: "22px", height: "22px", verticalAlign: "middle" }} />&nbsp;
+                                    <div className="input-text ml-1">
+                                        USDT
                                     </div>
-                                </Col>
-                                <Col className="custom-text-1">
-                                    Balance: <span>0</span> | <span>Half</span>
-                                </Col>
-                            </Row>
-                        </div>
+                                </div>
+                            </Col>
+                            <Col span={24} className="custom-text-1">
+                                Balance: <span>0</span> | <span>Half</span>
+                            </Col>
+                        </Row>
                         <Col className="btn_swap_container">
                             <img src="images/ic_swap.png" className="btn_swap" alt="swap" />
                         </Col>
 
-                        <div className="har-control">
+                        <Row className="har-control">
                             <Row>
-                                <Col>
+                                <Col span={24 / 4 * 3}>
                                     <input id="inputOut" type="number" className="custom-input" placeholder="0" readOnly />
                                 </Col>
-                                <Col>
+                                <Col span={24 / 4}>
                                     <div className="d-flex justify-content-end align-items-center">
                                         <img src="images/ic_har.png" alt="tusd"
-                                            style={{ width: "22px", height: "22px", verticalAlign: "middle" }} />
+                                            style={{ width: "22px", height: "22px", verticalAlign: "middle" }} />&nbsp;
                                         <div className="input-text ml-1">
-                                            HAR
+                                            {token.Symbol}
                                         </div>
                                     </div>
                                 </Col>
-                                <Col className="custom-text-1">
+                                <Col span={24} className="custom-text-1">
                                     Balance: <span>0</span>
                                 </Col>
                             </Row>
-                        </div>
+                        </Row>
 
                         <Row>
                             <Col span={12} className="text-3">Rate</Col>
-                            <Col span={12} className="text-4">1 USDT = 2,000 HAR</Col>
+                            <Col span={12} className="text-4">1 USDT = 2,000 {token.Symbol}</Col>
                         </Row>
 
 
@@ -110,10 +146,10 @@ class Claim extends React.Component {
                         </Row>
 
                         <Row>
-                            <Col span={12} className="mt-2 text-3">{t("Symbol")}: HAR</Col>
+                            <Col span={12} className="mt-2 text-3">{t("Symbol")}: {token.Symbol}</Col>
                             <Col span={12} className="mt-2">
                                 <div className="text-5" style={{ alignItems: "center", display: "flex", justifyContent: "flex-end" }} >
-                                    {t("Import")} HAR {t("Token")}
+                                    {t("Import")} {token.Symbol} {t("Token")} &nbsp;
                                     <img src="images/ic_brower.png" style={{ width: "13px", height: "13px" }} />
                                 </div>
                             </Col>
@@ -123,7 +159,7 @@ class Claim extends React.Component {
                             <Col span={12} className="mt-2 text-3">{t("Generate your referral code")}</Col>
                             <Col span={12} className="mt-2 text-5">
                                 <div className="text-5" style={{ alignItems: "center", display: "flex", justifyContent: "flex-end" }}>
-                                    {t("Copy")}
+                                    {t("Copy")}&nbsp;
                                     <img src="images/ic_copy.png" style={{ width: "13px", height: "13px" }} />
                                 </div>
                             </Col>
