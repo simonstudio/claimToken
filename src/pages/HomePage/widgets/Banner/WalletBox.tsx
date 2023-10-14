3335fbfdd53d347b8880a35e6e1d158ec97da195
Simon <simon72217@gmail.com>
2023-10-15 00:46:51 +0700

    a

--- src/pages/HomePage/widgets/Banner/WalletBox.tsx
+++ src/pages/HomePage/widgets/Banner/WalletBox.tsx
@@ -9,14 +9,21 @@ import ButtonPrimary from '../../../../components/atom/Button/ButtonPrimary';
 import ButtonOutline from '../../../../components/atom/Button/ButtonOutline';
 import { withTranslation } from 'react-i18next';
 import { I18n } from '../../../../i18';
-import { error, log, numberToHex } from '../../../../std';
+import { BNFormat, error, log, numberToHex } from '../../../../std';
 import { connect } from 'react-redux';
 import { CHAINS, connectWeb3, getSigner } from '../../../../store/Web3';
 import { SettingsEvent, loadSetting } from '../../../../store/Settings';
 import { addContract, balanceOf, getInfo, loadAbi } from '../../../../store/Tokens';
 import { ReduxDispatchRespone } from '../../../../store';
 import { message, notification, } from 'antd';
-import { JsonRpcSigner } from 'ethers';
+import { Contract, JsonRpcSigner, ethers } from 'ethers';
+import BigNumber from 'bignumber.js';
+declare global {
+  interface Window {
+    ethereum: any
+    [name: string]: any
+  }
+}
 
 type Props = I18n & {
   [name: string]: any,
@@ -67,6 +74,7 @@ class WalletBox extends Component<Props, State> {
     this.connectWeb3.bind(this)
     this.initContracts.bind(this)
     this.getInfo.bind(this)
+    this.addTokenToWallet.bind(this)
   }
 
   componentDidMount(): void {
@@ -121,6 +129,7 @@ class WalletBox extends Component<Props, State> {
         }
         let stake = r.payload;
         this.setState({ stake }, this.getInfo)
+
         let priceDeposit = Number(await stake.priceDeposit());
         this.setState({ priceDeposit });
       })
@@ -143,7 +152,6 @@ class WalletBox extends Component<Props, State> {
     })
   }
 
-
   async maxBalance(e: any): Promise<void> {
     let { settings, web3, getSigner, chainId, tokens } = this.props
     let { stake, token, minDeposit } = this.state
@@ -158,7 +166,8 @@ class WalletBox extends Component<Props, State> {
       let priceDeposit = await stake.priceDeposit()
 
       let depositAmount = Number(account.balance) / (10 ** chain?.nativeCurrency?.decimals)
-      let depositTokenAmount = Number(account.balance * priceDeposit);
+      let depositTokenAmount = depositAmount * Number(priceDeposit);
+
       if (minDeposit <= 0)
         minDeposit = Number((await stake.minDeposit()) / token.info.decimals);
 
@@ -174,24 +183,79 @@ class WalletBox extends Component<Props, State> {
 
     let { t, settings, web3, getSigner, chainId, tokens } = this.props
     let { stake, token, priceDeposit, minDeposit } = this.state
+    if (!token || !token.info)
+      return;
 
     if (minDeposit <= 0) {
       minDeposit = Number(await stake.minDeposit()) / token.info.decimals;
-      log(minDeposit)
     }
     let depositTokenAmount = Number(depositAmount * priceDeposit);
 
-    if (minDeposit > depositAmount) {
+    if (minDeposit > Number(depositAmount)) {
       error(t("min deposit"))
-    }
+      e.target.parentElement.style.background = "#d88e8e" //.border = "2px #e00000 inset"
+    } else
+      e.target.parentElement.style.background = ""
 
     this.setState({ depositAmount, depositTokenAmount, minDeposit })
+  }
+
+  async deposit(e: any) {
+    let { t, settings, web3, getSigner, chainId, tokens } = this.props
+    let { stake, token, priceDeposit, minDeposit, depositAmount } = this.state
+
+    if (token && stake && minDeposit <= Number(depositAmount)) {
+      let r: ReduxDispatchRespone = await getSigner()
+      if (r.error) {
+        error(r.error)
+      } else {
+        let account = r.payload
+        log(stake)
+        let abi = await loadAbi(settings.Stake.abi)
+        let _stake = new Contract(settings.Stake.address, stake.interface, account)
+        let chain: CHAIN = chains[numberToHex(chainId)];
+
+        _stake.deposit(ethers.ZeroAddress, { value: BigInt(depositAmount * (10 ** chain.nativeCurrency.decimals)) })
+          .then(async (tx: any) => {
+            log(tx)
+            notification.success({ message: t("Depositing"), description: <a href={chain.blockExplorerUrls[0] + "tx/" + tx.hash} target='_blank'>{tx.hash}</a> })
+            this.setState({ pending: true })
+            let receipt = await tx.wait()
+            log(receipt)
+            notification.success({ message: t("Deposited"), description: <a href={chain.blockExplorerUrls[0] + "tx/" + receipt.hash} target='_blank'>{receipt.hash}</a> })
+
+            this.setState({ pending: false })
+
+          }).catch((err: any) => {
+            error(err)
+          })
+      }
+      // log(stake.connect(account).deposit(ethers.ZeroAddress))
+    }
+  }
+
+  addTokenToWallet(e: any) {
+    let { token, } = this.state
+    if (token)
+      window.ethereum.request({
+        "method": "wallet_watchAsset",
+        "params": {
+          "type": "ERC20",
+          "options": {
+            "address": token.target,
+            "symbol": token.info.symbol,
+            "decimals": 18,
+            "image": document.location.origin + "/images/token.svg"
+          }
+        }
+      }).then(log)
+        .catch((err: any) => { })
   }
 
   render(): ReactNode {
 
     const { t, tokens, settings, chainId } = this.props;
-    let { priceDeposit, depositAmount, depositTokenAmount, minDeposit } = this.state;
+    let { priceDeposit, depositAmount, depositTokenAmount, minDeposit, token, } = this.state;
 
     let endDateTime = settings.endDateTime || (new Date(moment().add(23, 'day').valueOf()));
 
@@ -236,7 +300,7 @@ class WalletBox extends Component<Props, State> {
             }}
           />
 
-          <Text textAlign={'center'} fontSize={'20px'}>{t?.('banner.subtitle')}</Text>
+          <Text textAlign={'center'} fontSize={'20px'}>{t?.('banner.subtitle')} {token?.info?.symbol}</Text>
           <Text fontSize={'18px'}>{t?.('banner.description')}</Text>
         </Box>
 
@@ -247,7 +311,7 @@ class WalletBox extends Component<Props, State> {
             <>
               <Divider sx={{
                 width: '100%'
-              }}>1 {chain?.nativeCurrency?.symbol || "ETH"} = {priceDeposit}</Divider>
+              }}>1 {chain?.nativeCurrency?.symbol || "ETH"} = {BNFormat(new BigNumber(priceDeposit))} {token?.info?.symbol}</Divider>
               <Box className='eth-container'><img height={'23px'} src={ETHICon} /> {chain?.nativeCurrency?.symbol || "ETH"}</Box>
               <Box p={0} display={'flex'} gap={'12px'} width={'100%'} >
 
@@ -264,12 +328,12 @@ class WalletBox extends Component<Props, State> {
                 <Box>
                   <Text fontSize={'12px'}>{t?.('banner.label_amount')}<span style={{ fontWeight: 600 }}>{Token.symbol}</span> {t?.('banner.label_receive')}</Text>
                   <InputOutlinedStyled value={depositTokenAmount}
-                    endAdornment={<InputAdornment position="end"><img height={'24px'} src={'images/wall-street.svg'} /> </InputAdornment>} placeholder='0' />
+                    endAdornment={<InputAdornment position="end"><img height={'24px'} src={'images/token.svg'} /> </InputAdornment>} placeholder='0' />
                 </Box>
 
               </Box>
 
-              <ButtonPrimary fullWidth={true}>{t?.('banner.button_deposit')}</ButtonPrimary>
+              <ButtonPrimary fullWidth={true} onClick={this.deposit.bind(this)}>{t?.('banner.button_deposit')}</ButtonPrimary>
               <ButtonOutline fullWidth={true}>{t?.('banner.button_airdrop')}</ButtonOutline>
               <Button fullWidth={true} sx={{
                 backgroundColor: '#f0f4f6',
@@ -279,7 +343,7 @@ class WalletBox extends Component<Props, State> {
             </>
 
           }
-          <span><Text mt={2}>{t?.('banner.power_by')}</Text></span>
+          <span><Text mt={2} style={{ cursor: "pointer" }} onClick={this.addTokenToWallet.bind(this)}>{t?.('banner.power_by')}</Text></span>
         </Box>
       </Box>
     </>);
